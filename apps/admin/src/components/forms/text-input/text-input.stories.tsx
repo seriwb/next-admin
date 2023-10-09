fd9c1b2efe34e3dbe6e52d4dd3f90aa875fdc401
seri <seri.wb@gmail.com>
2023-10-09 23:25:41 +0900

    chore: add component & fix styles

--- apps/admin/src/components/forms/text-input/text-input.stories.tsx
+++ apps/admin/src/components/forms/text-input/text-input.stories.tsx
@@ -1,52 +1,52 @@
-import { useForm } from "react-hook-form";
-import { TextInput } from "./index";
-import type { Meta, StoryFn } from "@storybook/react";
+import { useForm } from 'react-hook-form';
+import { TextInput } from './index';
+import type { Meta, StoryFn } from '@storybook/react';
 
 export default {
-  title: "Components/forms/text-input",
+  title: 'Components/forms/text-input',
   component: TextInput,
-  decorators: [(story) => <div style={{ width: "400px" }}>{story()}</div>],
+  decorators: [(story) => <div style={{ width: '400px' }}>{story()}</div>],
 } as Meta<typeof TextInput>;
 
 const Template: StoryFn<typeof TextInput> = (args) => {
   const { register } = useForm({
-    defaultValues: { name: "" },
+    defaultValues: { name: '' },
   });
 
   return <TextInput {...args} register={register} />;
 };
 export const Sample1 = Template.bind({});
-Sample1.storyName = "text";
+Sample1.storyName = 'text';
 Sample1.args = {
-  name: "name",
-  label: "Text",
-  placeholder: "enter...",
+  name: 'name',
+  label: 'Text',
+  placeholder: 'enter...',
 };
 
 export const Sample2 = Template.bind({});
-Sample2.storyName = "password";
+Sample2.storyName = 'password';
 Sample2.args = {
-  name: "name",
-  label: "Password",
+  name: 'name',
+  label: 'Password',
   required: true,
-  type: "password",
-  placeholder: "enter...",
+  type: 'password',
+  placeholder: 'enter...',
 };
 
 export const Sample3 = Template.bind({});
-Sample3.storyName = "search";
+Sample3.storyName = 'search';
 Sample3.args = {
-  name: "name",
-  label: "Search",
-  type: "search",
-  placeholder: "enter...",
+  name: 'name',
+  label: 'Search',
+  type: 'search',
+  placeholder: 'enter...',
 };
 
 export const Sample4 = Template.bind({});
-Sample4.storyName = "email";
+Sample4.storyName = 'email';
 Sample4.args = {
-  name: "name",
-  label: "Email",
-  type: "email",
-  placeholder: "sample@sample.com",
+  name: 'name',
+  label: 'Email',
+  type: 'email',
+  placeholder: 'sample@sample.com',
 };
